f4d31c7a79f1e378520514eac85a9b4d94b86520
Abhay Mishra <abhaymishra0902@gmail.com>
2024-02-05 17:10:05 +0530

    added the feature of getting the data from the API and then spreading it in the screen also added RefreshIndicator

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -9,11 +12,38 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List items = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
+      ),
+      body: Visibility(
+        visible:isLoading ,
+        child: Center(child: CircularProgressIndicator(),),
+
+        replacement: RefreshIndicator(
+          onRefresh:fetchTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              return ListTile(
+                leading:CircleAvatar(child: Text('${index+1}'),),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+              );
+            },
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
@@ -23,11 +53,33 @@ class _TodoListPageState extends State<TodoListPage> {
       ),
     );
   }
-}
 
-void navigateToAddPage(BuildContext context) {
-  final route = MaterialPageRoute(
-    builder: (context) => AddTodoPage(),
-  );
-  Navigator.push(context, route);
+  void navigateToAddPage(BuildContext context) {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+
+
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=20';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+
+      // Assuming you have a state variable to store the fetched data
+      setState(() {
+        // Update your state variable with the fetched data
+        items = result;
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
 }
