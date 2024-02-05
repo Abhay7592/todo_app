a6654ae0b03258cb454c997d42cedc465a994961
Abhay Mishra <abhaymishra0902@gmail.com>
2024-02-05 16:00:59 +0530

    sending data to the server

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +11,9 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +24,14 @@ class _AddTodoPageState extends State<AddTodoPage> {
         padding: EdgeInsets.all(20),
         children: [
           TextField(
+            controller: titleController,
             decoration: InputDecoration(hintText: 'Title'),
           ),
           SizedBox(
             height: 20,
           ),
           TextField(
+            controller: descriptionController,
             decoration: InputDecoration(hintText: 'Description'),
             keyboardType: TextInputType.multiline,
             maxLines: 8,
@@ -32,13 +40,42 @@ class _AddTodoPageState extends State<AddTodoPage> {
           SizedBox(
             height: 20,
           ),
-          ElevatedButton(onPressed: () {}, child: Text('Submit'))
+          ElevatedButton(onPressed: submitData, child: Text('Submit'))
         ],
       ),
     );
   }
 
-  void submitData(){
+  Future<void> submitData() async {
+    // Get the data from the form
+    final title = titleController.text;
+    final description = descriptionController.text;
 
+    // Create a Map to represent the request body
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    // Submit the data to the server
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    // Show success or fail message based on status
+    if(response.statusCode == 201){
+      print('Creation Success');
+    }
+    else{
+      print('Creation Failed');
+      print(response.body);
+
+    }
   }
+
 }
